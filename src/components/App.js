a103181b4f2b1767270ad60f475148746ae1d40d
roobina90 <roobina1990@gmail.com>
2017-07-10 10:43:02 +0200

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+
+
+export default class Root extends Component {
+	render() {
+		return (
+			<span>Hello!</span>
+		);
+	}
+}
