819cab5231f5af7fdf2148d9ffe4916ef730c8b7
roobina90 <roobina1990@gmail.com>
2017-07-10 12:14:48 +0200

    get shops json

--- src/actions/shop.js
+++ src/actions/shop.js
@@ -1,4 +1,7 @@
 export const FETCH_SHOP_STARTED = 'FETCH_SHOP_STARTED';
+export const FETCH_SHOP_SUCCESS = 'FETCH_SHOP_SUCCESS';
+export const FETCH_SHOP_ERROR = 'FETCH_SHOP_ERROR';
+export const SET_SELECTED_SHOP = 'SET_SELECTED_SHOP';
 
 export function fetchShopData() {
     return (dispatch) => {
--- tools/server.js
+++ tools/server.js
@@ -1,30 +1,39 @@
-import express from 'express';  
-import webpack from 'webpack';  
-import path from 'path';  
-import config from '../webpack.config.dev';  
-import open from 'open';  
-//import favicon from 'serve-favicon';
-//import fs from 'fs';
+import express from 'express';
+import webpack from 'webpack';
+import path from 'path';
+import config from '../webpack.config.dev';
+import open from 'open';
+import fs from 'fs';
 import bodyParser from 'body-parser';
-//import shops from '../shop.data';
 /* eslint-disable no-console */
 
-const port = 3000;  
-const app = express();  
+const port = 3000;
+const app = express();
 const compiler = webpack(config);
 
-app.use(require('webpack-dev-middleware')(compiler, {  
+app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
 }));
 
-app.use(require('webpack-hot-middleware')(compiler));  
+app.use(require('webpack-hot-middleware')(compiler));
 app.use(bodyParser.json());
-app.get('', function(req, res) {  
-  res.sendFile(path.join( __dirname, '../src/index.html'));
+
+app.get('/shops', function (req, res) {
+  fs.readFile('./shop.data.json', "utf8", (err, data) => {
+    if (err) {
+      throw err;
+    }
+    res.setHeader('Content-Type', "application/json");
+    res.json(JSON.parse(data));
+  });
 });
 
-app.listen(port, function(err) {  
+app.get('', function (req, res) {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, function (err) {
   if (err) {
     console.log(err);
   } else {
