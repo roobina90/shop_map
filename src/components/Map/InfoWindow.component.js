361dd9e639e14204c8a16d3bf2c1a399b010a87b
roobina90 <roobina1990@gmail.com>
2017-07-11 20:15:53 +0200

    refactor for InfoWindow component

--- src/components/Map/InfoWindow.component.js
+++ src/components/Map/InfoWindow.component.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
 
 
 
 export default class InfoWindow extends Component {
     constructor(props) {
         super(props);
+        this.infoWindow = null;
+        this.infoWindowNode = null;
+    }
+
+    componentDidMount() {
+        this.infoWindow = new google.maps.InfoWindow();
+        this.makeInfoWindowEvent(this.props.map, this.infoWindow, ReactDOM.findDOMNode(this.infoWindowNode), this.props.marker);
+        this.props.pass(this.infoWindow);
+    }
+
+
+    makeInfoWindowEvent(map, infowindow, contentString, marker) {
+        google.maps.event.addListener(marker, 'click', function () {
+            infowindow.setContent(contentString);
+            infowindow.open(map, marker);
+        });
     }
 
     render() {
-        return (<div>
+        return (<div ref={(info) => { this.infoWindowNode = info } }>
             <img src={this.props.data.logo} width="50px" alt={this.props.data.siec} />
             <hr />
             <div>piątek - {this.props.data.godziny[0].Friday}</div>
--- src/components/Map/Marker.component.js
+++ src/components/Map/Marker.component.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import ReactDOM from 'react-dom';
 import InfoWindow from './InfoWindow.component';
 
 
@@ -8,25 +7,30 @@ export default class Marker extends Component {
     constructor(props) {
         super(props);
         this.createMarker = this.createMarker.bind(this);
-        this.makeInfoWindowEvent = this.makeInfoWindowEvent.bind(this);
-        this.infoWindowRef = null;
+       // this.makeInfoWindowEvent = this.makeInfoWindowEvent.bind(this);
+        this.getInfoWindow = this.getInfoWindow.bind(this);
+        this.infoWindow = null;
+        this.marker = null;
     }
 
 
-    makeInfoWindowEvent(map, infowindow, contentString, marker) {
-        google.maps.event.addListener(marker, 'click', function () {
-            infowindow.setContent(contentString);
-            infowindow.open(map, marker);
-        });
-    }
+    // makeInfoWindowEvent(map, infowindow, contentString, marker) {
+    //     google.maps.event.addListener(marker, 'click', function () {
+    //         infowindow.setContent(contentString);
+    //         infowindow.open(map, marker);
+    //     });
+    // }
 
     componentDidUnMount() {
         //google.maps.event.clearListeners(this.map, 'zoom_changed')
     }
 
-    createMarker() {
-        var infowindow = new google.maps.InfoWindow();
-        var marker = new google.maps.Marker({
+    getInfoWindow(windowObj) {
+        this.infoWindow = windowObj;
+    }
+
+    componentWillMount() {
+        this.marker = new google.maps.Marker({
             position: new google.maps.LatLng(this.props.data.latitude, this.props.data.longitude),
             map: this.props.map,
             icon: {
@@ -36,14 +40,16 @@ export default class Marker extends Component {
                 anchor: new google.maps.Point(0, 0)
             }
         });
-        this.makeInfoWindowEvent(this.map, infowindow, ReactDOM.findDOMNode(this.infoWindowRef), marker);
-        this.props.pass(marker, this.props.data.id, infowindow);
     }
+    createMarker() {
+        this.props.pass(this.marker, this.props.data.id, this.infoWindow);
+    }
+    
     componentDidMount() {
         this.createMarker();
     }
 
-    render() {
-        return (<InfoWindow data={this.props.data} ref={(infoWindow) => { this.infoWindowRef = infoWindow; }} />);
+    render() { 
+        return (<InfoWindow pass={this.getInfoWindow} marker={this.marker} map={this.props.map} data={this.props.data} />);
     }
 }
